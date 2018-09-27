63faa65fb985652dd4900f55cda975ec6901a501
Mikhail Shutov <mshutov@wiley.com>
2018-09-27 15:42:59 +0300

    new TranslationController added

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,39 +1,12 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class DemoController {
-    private final TranslationService translationService;
-
-    public DemoController(TranslationService translationService) {
-        this.translationService = translationService;
-    }
-
     @GetMapping
     public String hello() {
         return "Hello world!";
     }
-
-    @PostMapping(path = "/translations")
-    public void createTranslation(@RequestParam String word, @RequestParam String meaning) {
-        translationService.addTranslation(word, meaning);
-    }
-
-    @GetMapping(path = "/translations")
-    public Map<String, String> getTranslations() {
-        return translationService.findAll();
-    }
-
-    @GetMapping(path = "/translations/{word}")
-    public String getTranslations(@PathVariable String word) {
-        return translationService.findByWord(word).orElse("[No translation]");
-    }
-
 }
---
+++ src/main/java/com/example/demo/TranslationsController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:mshutov@wiley.com">Mikhail Shutov</a>
+ */
+@RestController
+@RequestMapping(path = "/translations")
+public class TranslationsController {
+    private final TranslationService translationService;
+
+    public TranslationsController(TranslationService translationService) {
+        this.translationService = translationService;
+    }
+
+    @PostMapping
+    public void createTranslation(@RequestParam String word, @RequestParam String meaning) {
+        translationService.addTranslation(word, meaning);
+    }
+
+    @GetMapping
+    public Map<String, String> getTranslations() {
+        return translationService.findAll();
+    }
+
+    @GetMapping(path = "/{word}")
+    public String getTranslations(@PathVariable String word) {
+        return translationService.findByWord(word).orElse("[No translation]");
+    }
+}
