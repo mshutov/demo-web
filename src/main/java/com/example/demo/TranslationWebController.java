85c0b8f8e5ce5b0c781942252f09910b25851ed4
Mikhail Shutov <mshutov@wiley.com>
2018-10-08 14:41:46 +0300

    on a way of adding web

--- src/main/java/com/example/demo/TranslationWebController.java
+++ src/main/java/com/example/demo/TranslationWebController.java
@@ -2,14 +2,16 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author <a href="mailto:mshutov@wiley.com">Mikhail Shutov</a>
+ * @author Mikhail Shutov
  */
 @Controller
+@RequestMapping(path = "/")
 public class TranslationWebController {
 
-    @GetMapping("/")
+    @GetMapping
     String home() {
         return "index";
     }
