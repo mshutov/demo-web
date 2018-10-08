de6270973fafc124366d4cbdf59304d2da8996df
Mikhail Shutov <mshutov@wiley.com>
2018-10-08 14:49:44 +0300

    on a way of adding web

--- src/main/java/com/example/demo/TranslationWebController.java
+++ src/main/java/com/example/demo/TranslationWebController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TranslationWebController {
 
     @GetMapping
-    String home() {
+    public String home() {
         return "index";
     }
 }
