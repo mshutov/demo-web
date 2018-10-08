be1f961777b18ee203f0360974c5e3509f013746
Mikhail Shutov <mshutov@wiley.com>
2018-10-08 15:08:14 +0300

    on a way of adding web

--- src/main/java/com/example/demo/TranslationWebController.java
+++ src/main/java/com/example/demo/TranslationWebController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,9 +11,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(path = "/")
 public class TranslationWebController {
+    private final TranslationService translationService;
+
+    public TranslationWebController(TranslationService translationService) {
+        this.translationService = translationService;
+    }
 
     @GetMapping
     public String home() {
         return "index";
+    }
+
+    @GetMapping(path = "random")
+    public String home(Model model) {
+        translationService.random().ifPresent(tp -> {
+            model.addAttribute("word", tp.getWord());
+            model.addAttribute("meaning", tp.getMeaning());
+        });
+        return "card";
     }
 }
