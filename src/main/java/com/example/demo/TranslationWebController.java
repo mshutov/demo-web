f360f56808629e26271549c304eb2c3192ae4925
Mikhail Shutov <shmv@live.ru>
2018-10-11 07:52:47 +0300

    header as include

--- src/main/java/com/example/demo/TranslationWebController.java
+++ src/main/java/com/example/demo/TranslationWebController.java
@@ -20,12 +20,13 @@ public class TranslationWebController {
     }
 
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("title", "Main page");
         return "index";
     }
 
     @GetMapping(path = "random")
-    public String home(Model model) {
+    public String random(Model model) {
         translationService.random().ifPresent(tp -> {
             model.addAttribute("word", tp.getWord());
             model.addAttribute("meaning", tp.getMeaning());
