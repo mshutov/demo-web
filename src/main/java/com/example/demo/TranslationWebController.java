01e2080f5c3606026d7f6438457bc7397dff3e79
Mikhail Shutov <shmv@live.ru>
2018-10-12 23:49:08 +0300

    fix

--- src/main/java/com/example/demo/TranslationWebController.java
+++ src/main/java/com/example/demo/TranslationWebController.java
@@ -2,10 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Mikhail Shutov
@@ -34,7 +31,7 @@ public class TranslationWebController {
         return "card";
     }
 
-    @GetMapping(path = "new")
+    @RequestMapping(path = "new", method = {RequestMethod.GET, RequestMethod.POST})
     public String createForm(@RequestParam(required = false) String word,
                              @RequestParam(required = false) String meaning,
                              Model model) {
