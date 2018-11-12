9965ae8762658daebd0a09d725c85f61e0e98736
Mikhail Shutov <mshutov@wiley.com>
2018-11-12 11:34:37 +0300

    Yandex translation for word without definition in local DB

--- src/main/java/com/example/demo/YandexTranslator.java
+++ src/main/java/com/example/demo/YandexTranslator.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,13 +15,14 @@ import lombok.val;
  * @author Mikhail Shutov
  */
 @Service
+@Slf4j
 public class YandexTranslator {
     private final RestTemplate restTemplate = new RestTemplate();
 
     private final static String API_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate" +
             "?key={apiKey}" +
             "&text={text}" +
-            "&lang=en_ru";
+            "&lang=en-ru";
 
     private final String apiKey;
 
@@ -30,6 +32,7 @@ public class YandexTranslator {
 
     public Optional<String> findByWord(String word) {
         val url = UriComponentsBuilder.fromHttpUrl(API_URL).build(apiKey, word);
+        log.info("Url to query for translation: {}", url);
         return Optional.ofNullable(restTemplate.getForObject(url, YandexTranslation.class))
                 .flatMap(YandexTranslation::getFirstTranslation);
     }
